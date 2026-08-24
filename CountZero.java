abf52454b5512d91702f2951eaf07a40fa08a11d
Rajkiran1408 <rameshsundarraj72@gmail.com>
2026-08-24 23:21:02 +0530

    Count zero using recursions

---
+++ CountZero.java
@@ -0,0 +1,35 @@
+public class CountZero {
+
+    static int countZero(int n){
+        int c=0;
+        return helper(n,c);
+    }
+
+//    static int helper(int n , int c){
+//        if(n%10 == 0){
+//            c=c+1;
+//        }
+//        if(n%10 == n){
+//            return c;
+//        }
+//
+//        return helper(n/10,c);
+//    }
+
+    // better readability and simplicity
+    static int helper(int n , int c){
+        if(n==0){                         // id Total number is zero return count
+            return c;
+        }
+        if(n%10 == 0){                      // if the digit is zero , call and add count + 1
+            return helper(n/10,c+1);
+        }
+
+        return helper(n/10,c);         // Otherwise , call the method
+    }
+
+    static void main(String[] args) {
+        int n =300000010;
+        System.out.println(countZero(n));
+    }
+}
