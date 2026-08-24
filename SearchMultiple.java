7863644b34320c2324d5b51eb18495d05d092ab2
Rajkiran1408 <rameshsundarraj72@gmail.com>
2026-08-24 23:23:35 +0530

    Search same elements in mutiple times in array using recursions

---
+++ SearchMultiple.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class SearchMultiple {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,6,5,4};
+        int target = 4;
+        search(arr,target,0);
+        System.out.println(list);
+    }
+    static ArrayList<Integer> list = new ArrayList<>();
+    static public void search(int[] arr,int target, int index){
+
+        if(arr[index]==target){
+            list.add(index);
+        }
+        if(index==arr.length-1){
+            return;
+        }
+
+        search(arr,target, index+1);
+    }
+}
