78e93838e9ca6e396be37ed70d5e114adf81c4e7
Rajkiran1408 <rameshsundarraj72@gmail.com>
2026-08-24 23:22:34 +0530

    Search element in array using recursions

---
+++ SearchElements.java
@@ -0,0 +1,17 @@
+public class SearchElements {
+
+        public static void main(String[] args) {
+            int[] arr = {1,2,3,4,6,5};
+            int target = 41;
+            System.out.println(search(arr,target,0));
+        }
+
+        static public boolean search(int[] arr,int target, int index){
+
+            if(index==arr.length-1){
+                return false;
+            }
+            return arr[index]==target || search(arr,target, index+1);
+        }
+}
+
