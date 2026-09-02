72946b0ca58a634c4caa263c3bcd07fc7b5e07d9
Rajkiran1408 <rameshsundarraj72@gmail.com>
2026-09-02 19:54:51 +0530

    Count path start to end using recursions

---
+++ src/recursion/NoOfPath.java
@@ -0,0 +1,18 @@
+package src.recursion;
+
+public class NoOfPath {
+
+    static int pathCount(int r,int c){
+        if(r==1 || c==1){
+            return 1;
+        }
+
+        int left = pathCount(r-1,c);
+        int right = pathCount(r,c-1);
+
+        return left+right;
+    }
+    static void main(String[] args) {
+        System.out.println(pathCount(3,3));
+    }
+}
