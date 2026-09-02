f9496a136eeb5848fdd099af48d8eecd21b09f80
Rajkiran1408 <rameshsundarraj72@gmail.com>
2026-09-02 20:25:00 +0530

    Count path start to end using recursions

--- src/recursion/NoOfPath.java
+++ src/recursion/NoOfPath.java
@@ -1,5 +1,7 @@
 package src.recursion;
 
+import java.util.ArrayList;
+
 public class NoOfPath {
 
     static int pathCount(int r,int c){
@@ -29,9 +31,30 @@ public class NoOfPath {
 
     }
 
+    static ArrayList<String> pathRet(String s , int r, int c){
+        if(r==1 && c==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(s);
+            return list;
+        }
+
+        ArrayList<String> ans = new ArrayList<>();
+        if(r>1){
+            ans.addAll(pathRet(s+'D',r-1,c));
+        }
+
+        if(c>1){
+            ans.addAll(pathRet(s+'R',r,c-1));
+
+        }
+        return ans;
+
+    }
+
 
     static void main(String[] args) {
         System.out.println(pathCount(3,3));
         path("",3,3);
+        System.out.println(pathRet("",3,3));
     }
 }
